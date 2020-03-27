907f2d94fd15b356cf7662da3b384668657c4648
Manny Henri <mannyhenri@users.noreply.github.com>
2020-03-27 06:27:28 -0400

    updated main

--- HelloWorld/Main.cpp
+++ HelloWorld/Main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
-void Log(const char* message) 
-{
-	std::cout << message << std::endl;
-}
+void Log(const char* message);
 
 int main()
 {
+	int variable = 8; 
+	// if you add the keyword unsigned you can double max number with int 4,2 billion instead of 2,1 billion
+
 	Log("Hello World");
+	std::cout << variable << std::endl;
 	std::cin.get();
 }
